7f30c7a66d0d7eedfc985b1027467a70b7213759
robis24 <robis24@gmail.com>
2020-03-10 20:37:15 +0100

    cleanup

--- index.php
+++ index.php
@@ -1,26 +1,13 @@
 <?php
 
-// Inintialize URL to the variable
 $url = $_SERVER['REQUEST_URI'];
 
-// Use parse_url() function to parse the URL
-// and return an associative array which
-// contains its various components
 $url_components = parse_url($url);
-
-
-
-// Use parse_str() function to parse the
-// string passed via URL
 parse_str($url_components['query'], $params);
-
-// Display result
 
 if($params['mode'] == "edit"){
   $edit = "true";
 }
-
-
 
 ?>
 
